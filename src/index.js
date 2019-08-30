797d3a58cc44308522dc609d148784b19343273b
MrAnchor <dimsf97@gmail.com>
2019-08-30 21:37:07 +0300

    Adaptive header, footer, questions were done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './style.scss';
 
 import { listenSocial } from "./common.blocks/footer/footer";
-import { listClick } from "./common.blocks/questionsList/questions-list";
+import { listClick } from "./common.blocks/questions-list/questions-list";
 import { addQuestionsAskListener } from "./common.blocks/questions/questions";
 import { addModalCloseButtonListener } from "./common.blocks/modal/modal";
 
