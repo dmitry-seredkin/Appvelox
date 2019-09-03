c00bab35a1966184c88f62f8285bc8fd0725e14c
MrAnchor <dimsf97@gmail.com>
2019-09-03 17:11:40 +0300

    Main page was almost done; todo company js, refactor code maybe

--- src/common.blocks/crew/crew.js
+++ src/common.blocks/crew/crew.js
@@ -1,44 +1,34 @@
 
 export function addButtonsListeners() {
-    let index = 0;
-    let leftCoord = 0;
     const crew = document.querySelector('.crew');
     const max = Number(crew.dataset.count) - 1;
-    const slider = document.querySelector('.crew__slider');
-    const prevButton = slider.querySelector('.crew__button_prev');
-    const wrap = slider.querySelector('.crew__wrap');
-    const nextButton = slider.querySelector('.crew__button_next');
-    const circles = document.querySelectorAll('.crew__circle');
+    
+    const wrap = crew.querySelector('.crew__wrap');
+    const prevButton = crew.querySelector('.crew__button_prev');
+    const nextButton = crew.querySelector('.crew__button_next');
+    const circles = crew.querySelectorAll('.crew__circle');
+
+    let index = 0;
+    let leftCoordinate = 0;
 
     prevButton.addEventListener('click', () => {
         if (index > 0) {
-            circles[index].classList.remove('crew__circle_active');
-            if (--index === 0) {
-                prevButton.classList.add('crew__button_disabled');
-            }
-            if (index !== max) {
-                nextButton.classList.remove('crew__button_disabled');
-            }
-            leftCoord += 100;
-            wrap.style.left = `${leftCoord}%`;
-            circles[index].classList.add('crew__circle_active');
+            changeStyle(index, --index);
         }
-
     });
 
     nextButton.addEventListener('click', () => {
         if (index < max) {
-            circles[index].classList.remove('crew__circle_active');
-            if (++index === max) {
-                nextButton.classList.add('crew__button_disabled');
-            }
-
-            if (index !== 0) {
-                prevButton.classList.remove('crew__button_disabled');
-            }
-            leftCoord -= 100;
-            wrap.style.left = `${leftCoord}%`;
-            circles[index].classList.add('crew__circle_active');
+            changeStyle(index, ++index);
         }
     });
+
+    function changeStyle(oldIndex, index) {
+        circles[oldIndex].classList.remove('crew__circle_active');
+        prevButton.classList.toggle('crew__button_disabled', index === 0);
+        nextButton.classList.toggle('crew__button_disabled', index === max);
+        circles[index].classList.add('crew__circle_active');
+        leftCoordinate = leftCoordinate + (oldIndex - index) * 100;
+        wrap.style.left = `${leftCoordinate}%`;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import './style.scss';
 
 import { addSituationClickListener } from "./common.blocks/situation/situation";
 // import { listenSocial } from "./common.blocks/footer/footer";
-// import { listClick } from "./common.blocks/questions-list/questions-list";
-// import { addQuestionsAskListener } from "./common.blocks/questions/questions";
-// import { addModalCloseButtonListener } from "./common.blocks/modal/modal";
+import { listClick } from "./common.blocks/questions-list/questions-list";
+import { addQuestionsAskListener } from "./common.blocks/questions/questions";
+import { addModalCloseButtonListener } from "./common.blocks/modal/modal";
 //
 // listenSocial();
 // listClick();
 // addQuestionsAskListener();
 // addModalCloseButtonListener();
+//
+import { addButtonsListeners } from "./common.blocks/crew/crew";
 
-// import { addButtonsListeners } from "./common.blocks/crew/crew";
-
-// addButtonsListeners();
+addButtonsListeners();
 addSituationClickListener();
